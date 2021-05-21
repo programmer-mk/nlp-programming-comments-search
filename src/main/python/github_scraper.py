d051769eee71711d78eb5ff75d825a235839ab39
Milisav Kovacevic <mkovacevic@vast.com>
2021-05-21 22:53:56 +0200

    download files from github

--- src/main/python/github_scraper.py
+++ src/main/python/github_scraper.py
@@ -1,8 +1,30 @@
+import os
 import requests
 from collections import namedtuple
 
 GITHUB_AUTH_TOKEN = 'dummy'
 GithubItem = namedtuple('GithubItem', 'file_name file_url repository_name repository_owner')
+
+
+def compute_directory_name(phrase):
+    terms = phrase.split()
+    return '_'. join(terms)
+
+
+def create_directory_for_search_phrase(directory_name):
+    full_directory_path = f'../resources/{directory_name}'
+    if not os.path.exists(full_directory_path):
+        os.makedirs(full_directory_path)
+
+
+def download_files(github_files, search_phrase):
+    directory_name = compute_directory_name(search_phrase)
+    create_directory_for_search_phrase(directory_name)
+    for github_file in github_files:
+        url = github_file.file_url
+        headers = {'accept': 'application/vnd.github.VERSION.raw'}
+        r = requests.get(url, headers=headers)
+        open(f'../resources/{directory_name}/{github_file.file_name}', 'wb').write(r.content)
 
 
 def search_github(phrase, prog_language):
@@ -13,7 +35,7 @@ def search_github(phrase, prog_language):
     headers = {'Authorization': f'token {GITHUB_AUTH_TOKEN}'}
     r = requests.get(query_url, headers=headers, params=params)
     result_items = r.json()['items']
-    items = [GithubItem(t['name'], t['html_url'], t['repository']['full_name'], t['repository']['owner']['id']) for t in result_items]
+    items = [GithubItem(t['name'], t['git_url'], t['repository']['full_name'], t['repository']['owner']['id']) for t in result_items]
     return items
 
 
@@ -22,3 +44,4 @@ if __name__ == '__main__':
     search_phrase = input(f'Enter search phrase on {natural_language} language: ')
     programming_language = input('Enter programming language for code searching: ')
     files = search_github(search_phrase, programming_language)
+    download_files(files, search_phrase)
