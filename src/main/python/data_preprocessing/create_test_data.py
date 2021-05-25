3883e7a501eccf2dbb7ba2924cfe3cb92e741971
Milisav Kovacevic <mkovacevic@vast.com>
2021-05-25 20:51:21 +0200

    fix bag-of-words

---
+++ src/main/python/data_preprocessing/create_test_data.py
@@ -0,0 +1,18 @@
+# data = [
+#
+# with open('fname', 'w') as file:
+#     for nested_list in dict_with_lists.values():
+#         for word in nested_list:
+#             file.write(word + '\t')
+#         file.write('\n')
+#
+#
+# if __name__ == '__main__':
+#     file_name = '../resources/comments.txt'
+#     create_test_dataset()
+#
+#
+#
+#     /comments.txt", sep="\t", names=['ProgrammingLanguageName', 'QueryID',
+#     'PairID', 'QueryText', 'CommentText',
+#     'SimilarityScore'])
--- src/main/python/data_preprocessing/preprocessing.py
+++ src/main/python/data_preprocessing/preprocessing.py
@@ -2,17 +2,26 @@ import os
 import pandas as pd
 import re
 from sklearn.feature_extraction.text import CountVectorizer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize.toktok import ToktokTokenizer
 
 MAIN_CONFIG_DIR = '../../config'
 
-cv_unigram = CountVectorizer(ngram_range=(1, 1))
+# TODO: should we use (1, 2), (1,3) and (2,3) combinations
+# analyzer needed for multiple columns
+cv_unigram = CountVectorizer(ngram_range=(1, 1), analyzer=lambda x: x)
 cv_bigram = CountVectorizer(ngram_range=(2, 2))
 cv_trigram = CountVectorizer(ngram_range=(3, 3))
+tokenizer = ToktokTokenizer()
+
+
+def initialize_global_vectorizers(comments):
+     unigram_counts = cv_unigram.fit_transform(comments['CommentText'])
+     cv_bigram.fit(comments['CommentText'])
+     cv_trigram.fit(comments['CommentText'])
 
 
 def remove_stop_words_and_tokenize_word(text, stopwords):
-    words = word_tokenize(text)
+    words = tokenizer.tokenize(text)
     return "".join([word + " " for word in words if word not in stopwords.words()])
 
 
@@ -80,35 +89,31 @@ def without_preprocessing(data_set):
 
 def lowercasing(data_set):
     data_set['CommentText'] = data_set['CommentText'].apply(lambda comment: comment.lower())
+    data_set['QueryText'] = data_set['QueryText'].apply(lambda comment: comment.lower())
     return create_bag_of_words(data_set)
 
 
 processing_steps = {
-    1: without_preprocessing,
-    2: lowercasing,
-    3: stemming_and_remove_stopwords,
+    0: without_preprocessing,
+    1: lowercasing,
+    2: stemming_and_remove_stopwords,
 }
 
 
 def read_raw_data():
-    comments = pd.read_csv("../resources/comments.txt", sep="\t", names=['ProgrammingLanguageName', 'QueryID',
+    comments = pd.read_csv("../../resources/comments.csv", names=['ProgrammingLanguageName', 'QueryID',
                                                                          'PairID', 'QueryText', 'CommentText',
                                                                          'SimilarityScore'])
-    return comments[['CommentText', 'SimilarityScore']]
-
-
-def init_variables(comments):
-    unigram_counts = cv_unigram.fit_transform(comments['CommentText'])
-    cv_bigram.fit(comments['CommentText'])
-    cv_trigram.fit(comments['CommentText'])
+    return comments[['QueryText', 'CommentText']]
 
 
 def remove_outliers(data):
+    # TODO: implement outliers removal logic
     return data
 
 
 def create_bag_of_words(data_set):
-    return cv_unigram.transform(data_set['CommentText'])
+    return pd.DataFrame(cv_unigram.transform(data_set[['CommentText', 'QueryText']]).todense())
 
 
 def start_processing(step, data_set):
@@ -116,7 +121,8 @@ def start_processing(step, data_set):
     if processing_technique is None:
         print('Unrecognized processing type. Please specify number between 1 - x')
     else:
-        processing_technique(data_set)
+        processed_data = processing_technique(data_set)
+        print(processed_data)
 
 
 def remove_special_characters(text):
@@ -138,14 +144,15 @@ def init_preprocessing(data):
     # Remove special characters, outliers, duplicates, null values, html tags
     data = data.dropna()
     data['CommentText'] = data['CommentText'].apply(remove_special_characters)
+    data['QueryText'] = data['QueryText'].apply(remove_special_characters)
     data = remove_outliers(data)
     # labels are already encoded(assume SimilarityScore is integer)
     return data
 
 
 if __name__ == '__main__':
-    raw_data = read_raw_data()
-    init_variables(raw_data)
-    preprocessed_data = init_preprocessing(raw_data)
     for step in list(range(9)):
+        raw_data = read_raw_data()
+        initialize_global_vectorizers(raw_data)
+        preprocessed_data = init_preprocessing(raw_data)
         start_processing(step, preprocessed_data)
