d6306f93343b7c952ddbb10c101b8bd4c75617c8
Milisav Kovacevic <mkovacevic@vast.com>
2021-05-30 12:19:09 +0200

    read files

--- src/main/python/models/main.py
+++ src/main/python/models/main.py
@@ -1,5 +1,22 @@
 # imports
-from .classifiers import SVC
+import numpy as np
+import pandas as pd
+from classifiers import SVC
+
+RESOURCES_DIR = '../../resources/'
+PROCESSED_DATA_DIR = 'processed_data'
+RAW_DATA_DIR = 'raw_data'
+
+raw_data = None
+without_preprocessing_data = None
+lowercasing_data = None
+tf_data = None
+tf_idf_data = None
+stemm_stopwords_data = None
+frequency_filtering_data = None
+bigrams_data = None
+trigrams_data = None
+binary_bow_data = None
 
 
 def apply_all_classifiers(data):
@@ -10,8 +27,16 @@ def apply_all_classifiers(data):
     #logistic_regression.logistic_regression(data)
 
 
-def load_preprocessed_data(file):
-    pass
+def load_data():
+    without_preprocessing_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/without_preprocessing.txt')
+    lowercasing_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/lowercasing.txt')
+    tf_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/tf.txt')
+    tf_idf_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/tf_idf.txt')
+    stemm_stopwords_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/stemming_and_remove_stopwords.txt')
+    frequency_filtering_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/bigrams.txt')
+    bigrams_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/bigrams.txt')
+    trigrams_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/trigrams.txt')
+    binary_bow_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/binary_bow.txt')
 
 
 def classifying():
@@ -54,6 +79,7 @@ if __name__ == "__main__":
 
     # TODO: add comment annotation similarity @djojdanic @bselic
     if option == 0:
+        load_data()
         classifying()
     else:
         print('percentage annotation calculation!')
