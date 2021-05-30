5f1ee3e9795db21bec382dc363d5910dc86ad371
Milisav Kovacevic <mkovacevic@vast.com>
2021-05-30 13:41:01 +0200

    refactor svc

--- src/main/python/models/classifiers/SVC.py
+++ src/main/python/models/classifiers/SVC.py
@@ -1,25 +1,31 @@
-import pandas as pd
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold, GridSearchCV, train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.metrics import f1_score, classification_report, accuracy_score
 
 
-def train_model(train, test, fold_no, rf, c=1):
-    X = ['Retail_Price','Discount']
-    y = ['Returned_Units']
-    X_train = train[X]
+def train_test_init(train, test):
+    y = ['SimilarityScore']
     y_train = train[y]
-    X_test = test[X]
+    X_train = train.drop(y, axis = 1)
     y_test = test[y]
+    X_test = test.drop(y, axis = 1)
+    return X_train, y_train, X_test, y_test
 
-    svc = LinearSVC(penalty=rf, C=c, dual=rf == 'l2', max_iter=15000)
+
+def train_model(train, test, fold_no, rf, c=1):
+    y = ['SimilarityScore']
+    y_train = train[y]
+    X_train = train.drop(y, axis = 1)
+    y_test = test[y]
+    X_test = test.drop(y, axis = 1)
+
+    svc = LinearSVC(penalty=rf, C=c, dual=rf == 'l2')
     svc.fit(X_train,y_train)
     predictions = svc.predict(X_test)
     print('Fold',str(fold_no),'Accuracy:', accuracy_score(y_test,predictions))
 
 
-def compare_regularisation_functions(data_frame, rf, c=1):
+def compare_regularisation_functions(data_frame, rf):
     skf = StratifiedKFold(n_splits=10)
     target = data_frame.loc[:,'SimilarityScore']
 
@@ -31,7 +37,9 @@ def compare_regularisation_functions(data_frame, rf, c=1):
         fold_no += 1
 
 
-def optimize_c_parameter(X_train, y_train, X_test, y_test):
+def optimize_c_parameter(train, test):
+    X_train, y_train, X_test, y_test = train_test_init(train, test)
+
     # defining parameter range
     param_grid = {'C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]}
 
@@ -49,22 +57,16 @@ def optimize_c_parameter(X_train, y_train, X_test, y_test):
     print(classification_report(y_test, grid_predictions))
 
 
-def support_vector_classifier(comments_data, comments_similarity, type):
-    """
-    :param comments_data:  X_train + X_test
-    :param comments_relevancy:  y_train + y_test
-    :param type:
-    :return:
-    """
+def support_vector_classifier(comments_data):
     print("Support vector classifier")
-    data_frame = pd.read_csv(f'processed_data/{type}.txt')
 
     # Testing differences between regularisation functions
     print("> L1/L2 comparing")
-    compare_regularisation_functions(data_frame, 'l1')
-    compare_regularisation_functions(data_frame, 'l2')
+    compare_regularisation_functions(comments_data, 'l1')
+    compare_regularisation_functions(comments_data, 'l2')
 
     # Optimizing C parameter
     print("> Results with optimized C parameter")
-    optimize_c_parameter()
+    train, test = train_test_split(comments_data, test_size=0.2, random_state=42, shuffle=True)
+    optimize_c_parameter(train, test)
 
--- src/main/python/models/main.py
+++ src/main/python/models/main.py
@@ -18,6 +18,9 @@ bigrams_data = None
 trigrams_data = None
 binary_bow_data = None
 
+# SimilarityScore
+data_target_column = None
+
 
 def apply_all_classifiers(data):
     SVC.support_vector_classifier(data)
@@ -28,6 +31,9 @@ def apply_all_classifiers(data):
 
 
 def load_data():
+    global without_preprocessing_data, lowercasing_data, tf_data, tf_idf_data, stemm_stopwords_data,\
+        frequency_filtering_data, bigrams_data, trigrams_data, binary_bow_data
+
     without_preprocessing_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/without_preprocessing.txt')
     lowercasing_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/lowercasing.txt')
     tf_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/tf.txt')
@@ -39,37 +45,43 @@ def load_data():
     binary_bow_data = np.loadtxt(f'{RESOURCES_DIR}{PROCESSED_DATA_DIR}/binary_bow.txt')
 
 
+def load_target_column():
+    global data_target_column
+    data_target_column = pd.read_csv("../../resources/raw_data/comments.csv",
+                          names=['ProgrammingLanguageName', 'QueryID', 'PairID', 'QueryText', 'CommentText', 'SimilarityScore'])[['SimilarityScore']]
+
+
 def classifying():
     print("----------  No preprocessing(BOW) ----------")
-    apply_all_classifiers("without_preprocessing.txt")
+    apply_all_classifiers(without_preprocessing_data)
 
     print("----------  Lower casing ----------")
-    apply_all_classifiers("lowercasing.txt")
+    apply_all_classifiers(lowercasing_data)
 
     print("----------  Term Frequency ----------")
-    apply_all_classifiers("tf.txt")
+    apply_all_classifiers(tf_data)
 
     #double check this
     #print("----------  Inverse Document Frequency ----------")
     #apply_all_classifiers("idf.txt")
 
     print("----------  Term Frequency–Inverse Document Frequency ----------")
-    apply_all_classifiers("tf_idf.txt")
+    apply_all_classifiers(tf_idf_data)
 
     print("----------  Stemming and stopwords ----------")
-    apply_all_classifiers("stemming_and_remove_stopwords.txt")
+    apply_all_classifiers(stemm_stopwords_data)
 
     print("----------  Frequency word filtering ----------")
-    apply_all_classifiers("frequency_filtering.txt")
+    apply_all_classifiers(frequency_filtering_data)
 
     print("----------  Bigram preprocessing ----------")
-    apply_all_classifiers("bigrams.txt")
+    apply_all_classifiers(bigrams_data)
 
     print("----------  Trigram preprocessing ----------")
-    apply_all_classifiers("trigrams.txt")
+    apply_all_classifiers(trigrams_data)
 
     print("----------  Binary Bag of Words ----------")
-    apply_all_classifiers("binary_bow.txt")
+    apply_all_classifiers(binary_bow_data)
 
 
 if __name__ == "__main__":
@@ -79,6 +91,7 @@ if __name__ == "__main__":
 
     # TODO: add comment annotation similarity @djojdanic @bselic
     if option == 0:
+        load_target_column()
         load_data()
         classifying()
     else:
